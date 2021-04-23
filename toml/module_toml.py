56397e7f7eb27557d8df25c045ebc35a189bb161
QwertyForce <maximus.bystrov@gmail.com>
2021-04-23 17:23:46 +0300

    toml ready

--- toml/module_toml.py
+++ toml/module_toml.py
@@ -2,7 +2,10 @@ from toml_serializer import TomlSerializer
 
 o = {'head':{'a':"I'm a string",'b':["I'm","a","string"],'c':1212,'d':True}}
 
-f = open('somedata.toml','w')
-TomlSerializer().dump(o,f)
-f.close()
+
+data = TomlSerializer().dumps(o)
+
+obj = TomlSerializer().loads(data)
+print(type(obj))
+print(obj)
 
